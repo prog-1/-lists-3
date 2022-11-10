f39820374e20c1643675f812a5a683272b30c9ef
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-11-10 15:33:47 +0200

    Implemented merge

--- merge_sort.go
+++ merge_sort.go
@@ -1,17 +1,58 @@
 package main
 
+import "fmt"
+
 type node struct {
 	v    int
 	next *node
 }
 
-func merge(a, b *node) *node {
-	// TODO
-	return nil
+func SliceToList(s []int) (n *node) {
+	for i := len(s) - 1; i >= 0; i-- {
+		n = &node{s[i], n}
+	}
+	return n
+}
+func ListToSlice(l *node) []int {
+	s := make([]int, 0)
+	for cur := l; cur != nil; cur = cur.next {
+		s = append(s, cur.v)
+	}
+	return s
+}
+func Print(l *node) {
+	for i := l; i != nil; i = i.next {
+		fmt.Println(i.v)
+	}
+	fmt.Println()
+}
+
+// How to merge?
+// 1. Take the smallest element out of a and b
+// 2.
+func merge(a, b *node) (head *node) {
+	for cur := &head; a != nil || b != nil; cur = &(*cur).next {
+		if a != nil && b != nil && a.v < b.v { // a is smaller
+			*cur = a
+			a = a.next
+		} else if a != nil && b != nil && b.v < a.v { // b is smaller
+			*cur = b
+			b = b.next
+		} else if a == nil && b != nil {
+			*cur = b
+			b = b.next
+		} else if b == nil && a != nil {
+			*cur = a
+			a = a.next
+		}
+	}
+
+	return head
 }
 
 func split(l *node) (a, b *node) {
 	// TODO
+	fmt.Println("Smth")
 	return nil, nil
 }
 
@@ -21,4 +62,7 @@ func mergeSort(l *node) *node {
 }
 
 func main() {
+	a, b := SliceToList([]int{1, 4}), SliceToList([]int{2, 3})
+	l := merge(a, b)
+	Print(l)
 }
---
+++ merge_sort_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	type Want struct {
+		a, b []int
+	}
+	for _, tc := range []struct {
+		name string
+		init []int
+		want Want
+	}{
+		{"empty", []int{}, Want{[]int{}, []int{}}},
+		{"1", []int{1, 2}, Want{[]int{1}, []int{2}}},
+	} {
+		a, b := split(SliceToList(tc.init))
+		if s := ListToSlice(a); !reflect.DeepEqual(s, tc.want.a) {
+			t.Errorf("want = %v, got = %v", tc.want.a, s)
+		}
+		if s := ListToSlice(b); !reflect.DeepEqual(s, tc.want.b) {
+			t.Errorf("want = %v, got = %v", tc.want.b, s)
+		}
+	}
+}
+
+func TestMerge(t *testing.T) {
+	type Input struct {
+		a, b []int
+	}
+	for _, tc := range []struct {
+		name  string
+		input Input
+		want  []int
+	}{
+		{"empty", Input{[]int{}, []int{}}, []int{}},
+		{"1", Input{[]int{1}, []int{2}}, []int{1, 2}},
+	} {
+		got := merge(SliceToList(tc.input.a), SliceToList(tc.input.b))
+		if s := ListToSlice(got); !reflect.DeepEqual(s, tc.want) {
+			t.Errorf("want = %v, got = %v", tc.want, s)
+		}
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{"empty", []int{}, []int{}},
+		{"nil", nil, nil},
+		{"1", []int{4, 2, 1, 3}, []int{1, 2, 3, 4}},
+	} {
+		got := mergeSort(SliceToList(tc.input))
+		if s := ListToSlice(got); !reflect.DeepEqual(s, tc.want) {
+			t.Errorf("got = %v, want = %v", s, tc.want)
+		}
+	}
+}
